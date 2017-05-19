8c1d5c4d8894e2c12d692f3989453c44ecc98955
VictorPuig <vicpuigcer@gmail.com>
2017-05-19 12:01:54 +0200

    Preferences layout done

--- Battleship/src/ui/PreferencesPanel.java
+++ Battleship/src/ui/PreferencesPanel.java
@@ -30,61 +30,8 @@ public class PreferencesPanel extends JPanel{
 	public PreferencesPanel() {
 		setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 47, 384, 142);
-		add(panel);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1);
-		FlowLayout fl_panel_1 = new FlowLayout(FlowLayout.CENTER, 5, 10);
-		panel_1.setLayout(fl_panel_1);
-		
-		JLabel timeLabel = new JLabel("Time per turn");
-		timeLabel.setVerticalAlignment(SwingConstants.TOP);
-		panel_1.add(timeLabel);
-		
-		JPanel subPanel_1 = new JPanel();
-		panel_1.add(subPanel_1);
-		subPanel_1.setLayout(new BoxLayout(subPanel_1, BoxLayout.Y_AXIS));
-		
-		infiniteBtn = new JRadioButton("Infinite");
-		infiniteBtn.setSelected(true);
-		infiniteBtn.setActionCommand(infiniteBtn.getText());
-		buttonGroup.add(infiniteBtn);
-		subPanel_1.add(infiniteBtn);
-		
-		prefixedBtn = new JRadioButton("Prefixed");
-		prefixedBtn.setActionCommand(prefixedBtn.getText());
-		buttonGroup.add(prefixedBtn);
-		subPanel_1.add(prefixedBtn);
-		
-		prefixedComboBox = new JComboBox();
-		prefixedComboBox.setEnabled(false);
-		prefixedComboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}));
-		subPanel_1.add(prefixedComboBox);
-		
-		JPanel panel_2 = new JPanel();
-		panel.add(panel_2);
-		
-		JLabel modeLabel = new JLabel("Turn mode");
-		panel_2.add(modeLabel);
-		
-		JPanel subPanel_2 = new JPanel();
-		panel_2.add(subPanel_2);
-		subPanel_2.setLayout(new BoxLayout(subPanel_2, BoxLayout.Y_AXIS));
-		
-		classicBtn = new JRadioButton("Classic");
-		classicBtn.setSelected(true);
-		buttonGroup_1.add(classicBtn);
-		subPanel_2.add(classicBtn);
-		
-		alternativeBtn = new JRadioButton("Alternative");
-		buttonGroup_1.add(alternativeBtn);
-		subPanel_2.add(alternativeBtn);
-		
 		JPanel buttonPanel = new JPanel();
-		buttonPanel.setBounds(88, 200, 201, 38);
+		buttonPanel.setBounds(28, 220, 201, 38);
 		add(buttonPanel);
 		
 		closePrefBtn = new JButton("Close");
@@ -94,6 +41,45 @@ public class PreferencesPanel extends JPanel{
 		savePrefBtn = new JButton("Save");
 		savePrefBtn.setActionCommand("SavePrefBtn");
 		buttonPanel.add(savePrefBtn);
+		
+		JLabel timeLabel = new JLabel("Time per turn");
+		timeLabel.setBounds(53, 76, 103, 15);
+		add(timeLabel);
+		timeLabel.setVerticalAlignment(SwingConstants.TOP);
+		
+		JLabel modeLabel = new JLabel("Turn mode");
+		modeLabel.setBounds(53, 169, 103, 15);
+		add(modeLabel);
+		
+		infiniteBtn = new JRadioButton("Infinite");
+		infiniteBtn.setBounds(156, 51, 75, 23);
+		add(infiniteBtn);
+		infiniteBtn.setSelected(true);
+		infiniteBtn.setActionCommand(infiniteBtn.getText());
+		buttonGroup.add(infiniteBtn);
+		
+		prefixedBtn = new JRadioButton("Prefixed");
+		prefixedBtn.setBounds(156, 72, 83, 23);
+		add(prefixedBtn);
+		prefixedBtn.setActionCommand(prefixedBtn.getText());
+		buttonGroup.add(prefixedBtn);
+		
+		prefixedComboBox = new JComboBox();
+		prefixedComboBox.setBounds(160, 99, 65, 23);
+		add(prefixedComboBox);
+		prefixedComboBox.setEnabled(false);
+		prefixedComboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}));
+		
+		classicBtn = new JRadioButton("Classic");
+		classicBtn.setBounds(157, 148, 74, 23);
+		add(classicBtn);
+		classicBtn.setSelected(true);
+		buttonGroup_1.add(classicBtn);
+		
+		alternativeBtn = new JRadioButton("Alternative");
+		alternativeBtn.setBounds(156, 173, 103, 15);
+		add(alternativeBtn);
+		buttonGroup_1.add(alternativeBtn);
 
 	}
 	
